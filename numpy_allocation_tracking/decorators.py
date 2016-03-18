f7c590bef3208a48ac8bf95902fbaf480d8f4f16
Stuart Berg <bergs@janelia.hhmi.org>
2016-03-18 19:19:40 -0400

    decorators: Added assert_mem_usage_factor

--- numpy_allocation_tracking/decorators.py
+++ numpy_allocation_tracking/decorators.py
@@ -1,4 +1,11 @@
+import numpy as np
+
 from .track_allocations import AllocationTracker
+
+import os
+import functools
+import logging
+logger = logging.getLogger(__name__)
 
 def track_max_alloc(func):
     """
@@ -25,6 +32,7 @@ def track_max_alloc(func):
         1000
 
     """
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         wrapper.tracker = AllocationTracker()
         with wrapper.tracker:
@@ -37,3 +45,55 @@ def track_max_alloc(func):
     wrapper.__wrapped__ = func # Emulate python 3 behavior of @functools.wraps
     return wrapper
 
+def assert_mem_usage_factor(max_allowed_usage_factor=1.0, comparison_input_arg=0, memory_log_dir=None):
+    """
+    Decorator.
+    
+    Calls the given function and checks the memory usage of all numpy array allocations it makes.
+
+    The memory usage is compared to the size of one of the arguments, the first argument by default,
+    but this can be overridden by setting comparison_input_arg to an int or string (to select a keyword argument).
+
+    If the memory usage exceeds some multiple of the input data size (as specified by max_allowed_usage_factor),
+    then an assertion is raised and an html log file of numpy allocations is written out.
+    Set memory_log_dir to customize the location of the written log file.  By default, it's written to the CWD.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # Pick an input argument to compare usage against.
+            if isinstance(comparison_input_arg, int):
+                input_data = args[comparison_input_arg]
+            elif isinstance(comparison_input_arg, str):
+                input_data = kwargs[comparison_input_arg]
+            assert isinstance(input_data, np.ndarray)
+    
+            func_name = func.__name__
+            if isinstance(func, functools.partial):
+                func_name = func.func.__name__
+            
+            memtracked_func = track_max_alloc(func)
+            result = memtracked_func(*args, **kwargs)
+    
+            mem_usage_mb = memtracked_func.max_array_usage / 1e6
+            mem_usage_factor = memtracked_func.max_array_usage / float(input_data.nbytes)
+    
+            logger.debug( "Memory usage factor of {}(): {:.1f}x".format(func.__name__, mem_usage_factor) )
+        
+            try:
+                output_dir = memory_log_dir or os.getcwd()
+                usage_log_path = os.path.join(output_dir, func.__name__ + '-allocation-log.html')
+                assert mem_usage_factor <= max_allowed_usage_factor, \
+                    "Max memory usage of '{}' was too high: {:.1f} MB ({:.1f}x of input)\n"\
+                    "Writing mem_usage_log to {}\n"\
+                    .format( func_name, mem_usage_mb, mem_usage_factor, usage_log_path )
+            except AssertionError:
+                memtracked_func.tracker.write_html(usage_log_path)
+                raise
+        
+            return result
+
+        wrapper.__wrapped__ = func # Emulate python 3 behavior of @functools.wraps
+        return wrapper
+    return decorator
